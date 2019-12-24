bb404b85152927b0b5f6c4af7021f0abb172735b
Anastasiia Koval <58176117+NastiaKoval@users.noreply.github.com>
2019-12-24 18:53:57 +0200

    Add files via upload

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,34 @@
+'use strict';
+let a, b, c;
+a = prompt('Please, input value for a', '1');
+if (a===null || isNaN(Number(a)) || Number(a)===0) {
+  console.log('Invalid input data');
+} else {
+    a = Number(a);
+    b = prompt('Please, input value for b', '1');
+    if (b===null || isNaN(Number(b))) {
+      console.log('Invalid input data');
+    } else {
+        b = Number(b);
+        c = prompt('Please, input value for c', '1');
+        if (c===null || isNaN(Number(c))) {
+        console.log('Invalid input data');
+        } else {
+            c = Number(c);
+            const two = 2;
+            const four = 4;
+            let discr = Math.pow(b, two) - four*a*c;
+            if (discr<0) {
+              console.log('no solution');
+            } else if (discr>0) {
+                let x1 = (-b + Math.sqrt(discr))/(two*a);
+                let x2 = (-b - Math.sqrt(discr))/(two*a);
+                console.log('x1 =', x1);
+                console.log('and x2 =', x2);  
+            } else {
+                let x = -b/(two*a);
+                console.log('x =', x);
+            }            
+        }
+    }
+}
---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,23 @@
+'use strict';
+let a, b, c;
+a = prompt('Please, input value for the first side of triangle', '1');
+b = prompt('Please, input value for the second side of triangle', '1');
+c = prompt('Please, input value for the third side of triangle', '1');
+if (isNaN(Number(a)) || isNaN(Number(b)) || isNaN(Number(c)) || a===null || b===null || c===null) {
+  alert('Input values should be ONLY numbers');
+} else if (Number(a)<=0 || Number(b)<=0 || Number(c)<=0 ) {
+  alert('A triangle must have 3 sides with a positive definite length');
+} else {
+  a = parseInt(a);
+  b = parseInt(b);
+  c = parseInt(c);
+  if (a+b<=c || a+c<=b || b+c<=a) {
+    console.log('Triangle doesn’t exist');
+  } else if (a===b && b===c) {
+    console.log('Equilateral triangle');         
+  } else if (a===b || b===c || c===a) {
+    console.log('Isosceles triangle'); 
+  } else {
+    console.log('Scalene triangle');
+  }
+}
